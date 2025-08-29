5ce154938bcc54f1c7961eef1edfa0c9543c1e2c
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-29 16:50:29 -0300

    atividade

---
+++ Condicionais/operações básicas.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+n1= int(input("Digite o primeiro número "))
+n2= int(input("Digite o segundo número "))
+c= float("Digite o caractere da operação desejada ")
+
+s= n1 - n2
+m= n1 * n2
+d= n1 / n2
+a= n1 + n2
+
+if c = s
+print ("O resultado é {s}")
