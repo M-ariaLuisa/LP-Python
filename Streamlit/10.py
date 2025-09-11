473d0e1916a07e50e02751ab51f954b36d53511d
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-11 14:59:02 -0300

    atividade

---
+++ Streamlit/10.py
@@ -0,0 +1,17 @@
+# Escreva um algoritmo que solicite do usuário um número
+# e mostre a tabuada de multiplicação do número informado
+
+import streamlit as st
+import time
+
+st.title("Atividade 3")
+st.header("Tabuada")
+
+numero=st.number_input("Digite um número")
+
+
+if st.button("Iniciar"):
+    for i in range(1,11):
+        st.info(f"{numero} x  {i} = {numero * i}")
+        time.sleep(1)
+    st.header("Fim")
