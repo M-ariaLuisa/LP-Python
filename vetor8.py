bc2906bcdc8fee250eb7be9d72bfc0500af06e37
M-ariaLuisa <luisaoliveira643@gmail.com>
2025-10-02 16:36:25 -0300

    Add files via upload

---
+++ vetor8.py
@@ -0,0 +1,23 @@
+import os
+os.system("cls")
+
+#Criando um vetor
+lista_numero = []
+#Definindo a quantidade de números a serem lidos
+QUANTIDADE_NUMEROS = 5
+
+
+
+
+for i in range(QUANTIDADE_NUMEROS):
+    numero = int(input(f"Digite o {i + 1}º número: "))
+    #Verificando se o número é par ou ímpar
+    if numero < 0:
+        lista_numero.append(0)
+    else:
+        lista_numero.append(numero)
+    
+print(f"Números digitados: {lista_numero}")
+
+
+
