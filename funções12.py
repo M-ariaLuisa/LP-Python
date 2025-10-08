651ea3717a31658b3fce9a936e71bf596963026c
M-ariaLuisa <luisaoliveira643@gmail.com>
2025-10-08 16:19:18 -0300

    Add files via upload

---
+++ funções12.py
@@ -0,0 +1,31 @@
+import os
+import time
+
+# Função sem parâmetros e sem retorno.
+def limpa_tela():
+    time.sleep(3) # Espera 3 segundos.
+    os.system("cls")
+
+# Função com parâmetros e com retorno.
+def multiplicar(n1):
+    return n1 * 100
+
+def mostrar_resultado(multiplicacao):
+    print(f"O resultado é {multiplicacao}")
+
+limpa_tela()
+metros = float(input("Informe a quantidade de metros: "))
+resultado = multiplicar(metros)
+mostrar_resultado(resultado)
+
+    
+
+
+
+
+
+
+
+
+
+
